2c049ed909a85bf2632815d76eac435b8a3a0b59
Christian Fredericksen <christian.fredericksen@yahoo.com>
2020-01-04 02:31:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,6 @@ class Book
   def initialize(title)
     @title = title
   end
-  
   def title
     @title 
   end
@@ -10,7 +9,6 @@ class Book
   def author=(author)
     @author = author
   end
-  
   def author
     @author
   end
@@ -21,5 +19,33 @@ class Book
   def page_count
     @page_count
   end
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
